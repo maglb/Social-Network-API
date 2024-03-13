b012667720adef47548b91a13dc8f5542753b36a
maglb <magali.lb21@gmail.com>
2024-03-13 17:43:47 -0600

    update user function added

---
+++ controllers/userController.js
@@ -0,0 +1,167 @@
+const { ObjectId } = require("mongoose").Types;
+const { User } = require("../models/User");
+
+// // Aggregate function to get the number of students overall
+// const headCount = async () => {
+//   const numberOfStudents = await Student.aggregate().count("studentCount");
+//   return numberOfStudents;
+// };
+
+// // Aggregate function for getting the overall grade using $avg
+// const grade = async (studentId) =>
+//   Student.aggregate([
+//     // only include the given student by using $match
+//     { $match: { _id: new ObjectId(studentId) } },
+//     {
+//       $unwind: "$assignments",
+//     },
+//     {
+//       $group: {
+//         _id: new ObjectId(studentId),
+//         overallGrade: { $avg: "$assignments.score" },
+//       },
+//     },
+//   ]);
+
+module.exports = {
+  // Get all user
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+
+      //   const studentObj = {
+      //     students,
+      //     headCount: await headCount(),
+      //   };
+
+      res.json(users);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  // Get a single user
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({
+        _id: req.params.userId,
+      }).select("-__v");
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: "No user was found with that ID" });
+      }
+
+      res.json({
+        user,
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  // create a new user
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // Delete a user and remove them from the course
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndRemove({
+        _id: req.params.userId,
+      });
+
+      if (!user) {
+        return res.status(404).json({ message: "No such user exists" });
+      }
+
+      //   const course = await Course.findOneAndUpdate(
+      //     { students: req.params.studentId },
+      //     { $pull: { students: req.params.studentId } },
+      //     { new: true }
+      //   );
+
+      //   if (!course) {
+      //     return res.status(404).json({
+      //       message: "Student deleted, but no courses found",
+      //     });
+      //   }
+
+      res.json({ message: "User successfully deleted" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  //update user
+  async updateUser(req, res) {
+    try {
+      const video = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: "No user was found with this id!" });
+      }
+
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
+  // Add a friend to a user
+  async addFriend(req, res) {
+    console.log("A new friend has been added to your friend's list");
+    console.log(req.body);
+
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { friends: req.body } },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: "No user found with that ID :(" });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // Remove friend from a studuserent
+  async deleteFriend(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { friends: { friendId: req.params.friendId } } },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: "No user found with that ID :(" });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
