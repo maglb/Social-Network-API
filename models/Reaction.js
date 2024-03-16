fabf35f0f6d7fdb9b4eada25a507f5847a49d167
maglb <magali.lb21@gmail.com>
2024-03-16 14:56:16 -0600

    unnecessary code removed

--- models/Reaction.js
+++ models/Reaction.js
@@ -6,7 +6,7 @@ const reactionSchema = new Schema(
   {
     reactionId: {
       type: ObjectId,
-default: () => new ObjectId(),
+      default: () => new ObjectId(),
     },
     reactionBody: {
       type: String,
@@ -26,15 +26,10 @@ default: () => new ObjectId(),
   {
     toJSON: {
       getters: true,
+      virtuals: true,
     },
+    _id: false,
   }
 );
-
-// Create a virtual property `reactionCount` that gets the amount of reaction the thought has
-// Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
-reactionSchema.virtual("reactionCount").get(function () {
-  return this.reactions.length;
-});
-
 
 module.exports = reactionSchema;
